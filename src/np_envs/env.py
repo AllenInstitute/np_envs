0cf40e1c9f1007ef02fca56aa8e716d4e981e343
bjhardcastle <ben.hardcastle@alleninstitute.org>
2023-05-18 11:58:50 -0700

    Split up classes

--- src/np_envs/env.py
+++ src/np_envs/env.py
@@ -1,3 +1,11 @@
+"""
+>>> env = EnvPath('np_pipeline_qc')
+>>> env.conda
+
+>>> env = EnvPath('np_pipeline_qc', python_version='3.10.5')
+>>> env.venv.pip_cache == env.conda.pip_cache
+True
+"""
 from __future__ import annotations
 
 import configparser
@@ -11,10 +19,140 @@ import np_envs.config as config
 
 ON_WINDOWS: bool = sys.platform == 'win32'
 
+class EnvPython:
+    
+    project_root: pathlib.Path
+    
+    root: pathlib.Path
+    python: pathlib.Path
+    version: str = config.DEFAULT_PYTHON_VERSION
+    """Python version, e.g. '3.8.5' or '3.8.*'"""
+    
+    def __init__(self, project_root: pathlib.Path, python_version: str | None = None, **kwargs):
+        if python_version is not None:
+            self.version = python_version
+        self.project_root = np_config.normalize_path(project_root)
+        
+    def __repr__(self) -> str:
+        return f'{self.__class__.__name__}({self.root})'
+    
+    @property
+    def name(self) -> str:
+        return self.project_root.name
+
+    @property
+    def root(self) -> pathlib.Path:
+        return self.project_root / self.version.split('.*')[0] # can't have `*` in path
+    
+    def create(self, *args, **kwargs) -> None:
+        raise NotImplementedError
+    
+    def update(self, *args, **kwargs) -> None:
+        raise NotImplementedError
+    
+    
+class PipManaged(EnvPython):
+    
+    def create(self, *args, **kwargs) -> None:
+        if self.python.exists():
+            print(f'{self} already exists at {self.root}')
+            return
+        self.run_create_cmd(*args, **kwargs)
+        self.add_pip_config()
+    
+    def run_create_cmd(self, python_version: str, *args, **kwargs) -> None:
+        raise NotImplementedError
+    
+    @property
+    def pip_ini(self) -> pathlib.Path:
+        return self.root / 'pip.ini'
+
+    @property
+    def pip_cache(self) -> pathlib.Path:
+        return self.root.parent / 'pip_cache'
+    
+    @property 
+    def pip_ini_config(self) -> configparser.ConfigParser:
+        pip_ini_config = configparser.ConfigParser()
+        pip_ini_config.read_dict(
+            config.PIP_CONFIG.get(self.name),
+            config.PIP_CONFIG['default'],
+        )
+        pip_ini_config.set('global', 'cache-dir', np_config.normalize_path(self.pip_cache).as_posix())
+        return pip_ini_config
+    
+    def add_pip_config(self):
+        if not self.root.exists():
+            raise FileNotFoundError(f'Cannot add pip config: {self.root} does not exist')
+        if not self.pip_ini.parent.exists():
+            self.pip_ini.parent.mkdir(parents=True)
+        with open(self.pip_ini, 'w') as f:
+            self.pip_ini_config.write(f)
+
+    @property
+    def requirements(self) -> pathlib.Path:
+        return pathlib.Path(__file__).parent / 'requirements' / f'{self.name}.requirements.txt'
+        
+    def update(self, requirements: pathlib.Path | None = None, **kwargs) -> None:
+        if requirements is None:
+            requirements = self.requirements
+        if not requirements.exists():
+            raise FileNotFoundError(f'Cannot update {self}: {requirements} does not exist')
+        self.add_pip_config()
+        self.run_update_cmd(requirements, **kwargs)
+
+    def run_update_cmd(self, requirements: pathlib.Path) -> None:
+        subprocess.run(f'{self.python} -m pip install -r {requirements}', check=True)
+    
+    
+class Conda(PipManaged):
+    
+    @property
+    def root(self) -> pathlib.Path:
+        return super().root / 'conda'
+    
+    @property
+    def python(self) -> pathlib.Path:
+        return self.root / 'python.exe' if ON_WINDOWS else self.root / 'bin' / 'python'
+    
+    def run_create_cmd(self, *args, **kwargs) -> None:
+        subprocess.run(
+            f'conda create -p {self.root} python={self.version} -y --copy --no-shortcuts {" ".join(args)}',
+            check=True,
+            )
+    
+    
+class Venv(PipManaged):    
+    
+    @property
+    def root(self) -> pathlib.Path:
+        return super().root / '.venv'
+    
+    @property
+    def python(self) -> pathlib.Path:
+        return self.root / 'Scripts' / 'python.exe' if ON_WINDOWS else self.root / 'bin' / 'python'
+    
+    def run_create_cmd(self, *args, **kwargs) -> None:
+        # we need a python version to create the venv
+        # pyenv would be lighter weight, but it's not usually available on windows
+        conda_env = Conda(self.project_root, self.version)
+        if not conda_env.python.exists():
+            conda_env.create('--no-default-packages')
+        subprocess.run(
+            f'{conda_env.python} -m venv --copies {self.root} {" ".join(args)}',
+            check=True,
+            )
+
+    
 class EnvPath(pathlib.WindowsPath if ON_WINDOWS else pathlib.PosixPath): # type: ignore
     """
     >>> env = EnvPath('np_pipeline_qc')
     """
+    
+    _conda: Conda | None = None
+    _venv: Venv | None = None
+    
+    version: str = config.DEFAULT_PYTHON_VERSION
     
     def __new__(cls, env_name: str, **kwargs):
         path = config.ROOT / env_name
@@ -22,84 +160,30 @@ class EnvPath(pathlib.WindowsPath if ON_WINDOWS else pathlib.PosixPath): # type:
             print(f'Env {env_name} does not exist: build with `self.venv_create("3.8.*")')
         return super().__new__(cls, path, **kwargs)
     
+    def __init__(self, path: pathlib.Path, python_version: str | None = None):
+        if python_version is not None:
+            self.version = python_version
+            
     def __repr__(self) -> str:
         return super().__repr__()
     
     @property
     def python(self) -> pathlib.Path:
-        return self.venv_python if self.venv_python.exists() else self.conda_python
+        return self.venv.python if self.venv.python.exists() else self.conda.python
     
     @property
-    def venv_root(self) -> pathlib.Path:
-        return self / '.venv'
+    def conda(self) -> Conda:
+        if not self._conda:
+            self._conda = Conda(self, self.version)
+        return self._conda
     
     @property
-    def conda_root(self) -> pathlib.Path:
-        return self
+    def venv(self) -> Venv:
+        if not self._venv:
+            self._venv = Venv(self, self.version)
+        return self._venv
     
-    @property
-    def venv_python(self) -> pathlib.Path:
-        return self.venv_root / 'Scripts' / 'python.exe' if ON_WINDOWS else self / 'bin' / 'python'
     
-    @property
-    def venv_cache(self) -> pathlib.Path:
-        return self.venv_root / 'pip_cache'
-    
-    @property
-    def conda_python(self) -> pathlib.Path:
-        return self.conda_root / 'python.exe' if ON_WINDOWS else self / 'bin' / 'python'
-    
-    @property
-    def requirements(self) -> pathlib.Path:
-        return pathlib.Path(__file__).parent / 'requirements' / f'{self.name}.requirements.txt'
-    
-    def conda_create(self, python_version: str | None) -> None:
-        if not python_version:
-            python_version = config.DEFAULT_PYTHON_VERSION
-        if self.conda_python.exists():
-            print(f'Conda env already exists at {self}')
-            return
-        subprocess.run(
-            f'conda create -n {self.name} python={python_version} -p {self}',
-            check=True,
-            )
-        config.add_env(self.name)
-        self.add_pip_config()
-        
-    def venv_create(self, python_version: str | None, **kwargs) -> None:
-        if not python_version:
-            python_version = config.DEFAULT_PYTHON_VERSION
-        if self.venv_python.exists():
-            print(f'Venv already exists at {self}')
-        if not self.conda_python.exists():
-            self.conda_create(python_version)
-        subprocess.run(f'{self.conda_python} -m venv --copies {self.venv_root}', check=True)
-        self.venv_update(kwargs.get('requirements'))
-        
-    def pip_ini_config(self) -> configparser.ConfigParser:
-        pip_ini_config = configparser.ConfigParser()
-        pip_ini_config.read_dict(
-            config.PIP_CONFIG.get(self.name),
-            config.PIP_CONFIG['default'],
-        )
-        pip_ini_config.set('global', 'cache-dir', np_config.normalize(self.venv_cache))
-        return pip_ini_config
-    
-    def add_pip_config(self):
-        for root in (self.conda_root, self.venv_root):
-            if root.exists():
-                with open(root / 'pip.ini', 'w') as f:
-                    self.pip_ini_config().write(f)
-    
-    def venv_update(self, requirements: pathlib.Path | None = None, **kwargs) -> None:
-        self.add_pip_config()
-        if not requirements:
-            requirements = self.requirements
-        if not requirements.exists():
-            print(f'No requirements file found at {requirements}')
-            return
-        subprocess.run(f'{self.venv_python} -m pip install -r {requirements}', check=True)
-
 if __name__ == '__main__':
     import doctest
     doctest.testmod()
